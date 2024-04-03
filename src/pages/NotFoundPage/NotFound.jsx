c71ac66ec83a0b64a14cd850bcd80adc1ab18535
Feniuk <143640539+Feniuk@users.noreply.github.com>
2024-04-03 20:22:08 +0200

    update

---
+++ src/pages/NotFoundPage/NotFound.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import styles from "./NotFoundPage.module.css";
+
+function NotFoundPage() {
+  return (
+    <div className={styles.notContainer}>
+      <h1 className={styles.notTitle}>Page is Not Found</h1>
+      <p className={styles.notText}>
+        The page you are looking for does not exist.
+      </p>
+      <Link to="/" className={styles.notLink}>
+        Go back to Home
+      </Link>
+    </div>
+  );
+}
+
+export default NotFoundPage;
