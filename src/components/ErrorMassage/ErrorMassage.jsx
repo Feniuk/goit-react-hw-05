62580c334c3c8fd3436641b8a0508f4c9673b172
Feniuk <143640539+Feniuk@users.noreply.github.com>
2024-04-03 17:12:09 +0200

    basic commit

--- src/components/ErrorMassage/ErrorMassage.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./ErrorMassage.module.css";
-
-export const ErrorMassage = () => {
-  return <p className={styles.error}>Oops, something went wrong!</p>;
-};
-
-export default ErrorMassage;
